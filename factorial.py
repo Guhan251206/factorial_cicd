4788aa5808ed1ada3d65dace63c2b0cf26155491
Guhan <guhan251206@gmail.com>
2026-09-22 22:25:48 +0530

    Update factorial implementation

--- factorial.py
+++ factorial.py
@@ -1,7 +1,10 @@
 def factorial(n):
-    if n<0:
+    if n < 0:
         raise ValueError("Factorial is not defined for negative number")
-    r=1
-    for i in range(1,n+1):
-        r=r*i
-    return r
+
+    result = 1
+
+    for i in range(1, n + 1):
+        result = result * i
+
+    return result
